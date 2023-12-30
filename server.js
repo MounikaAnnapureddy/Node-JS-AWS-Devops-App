0dfa624797b413e4a7a1fbba3f6a269fd7f58883
MounikaAnnapureddy <105825694+MounikaAnnapureddy@users.noreply.github.com>
2023-12-29 22:30:13 -0600

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ const domainURL = process.env.DOMAIN;
 app.post("/submit-form", async (req, res) => {
   // Retrieve form data from the request body
   const formData = req.body;
-  const session = formsubmitted({
+  const session = stripe.formsubmitted({
     success_url: `${domainURL}/success?id={SUBMISSION_SESSION_ID}`,
     cancel_url: `${domainURL}/cancel`,
   });
